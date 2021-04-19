db4643e57728c6a71258dbdf084f8334565f7e23
LDY <74583481+dy858@users.noreply.github.com>
2021-04-19 21:18:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
-
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QLabel
 
 class MyApp(QWidget):
 
@@ -11,8 +13,34 @@ class MyApp(QWidget):
     def initUI(self):
         self.setWindowTitle('My First Application')
         self.move(300, 300)
-        self.resize(400, 200)
+        self.resize(900, 500)
+
+
+        self.pixmap = QPixmap('image.png')
+        self.label = QLabel(self)
+        self.label.setPixmap(self.pixmap)  # 이미지 세팅
+        self.label.setContentsMargins(10, 10, 10, 10)
+
+
+
+        remove_button = QPushButton("remove", self)
+        remove_button.move(380, 450)
+        remove_button.clicked.connect(self.Remove)
+
+        show_button = QPushButton("show", self)
+        show_button.move(500, 450)
+        show_button.clicked.connect(self.Show)
+
+        self.checkbox = QCheckBox("체크박스", self)
+        self.checkbox.move(100, 450)
+
         self.show()
+
+    def Remove(self):
+        self.image.close()
+
+    def Show(self):
+        self.image.show()
 
 
 if __name__ == '__main__':
