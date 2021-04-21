02a8ee63385956c1b0557f71a4c996851236fe6b
LDY <74583481+dy858@users.noreply.github.com>
2021-04-21 20:42:04 +0900

    gitignore

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ from PyQt5.QtGui import QPixmap
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        # 화면의 (x : 150, y: 50) 위치에서 (width : 300, height : 450) 크기의 윈도우가 생성됩니다.
-        self.setGeometry(150, 50, 1000, 450)
+        # 화면의 (x : 150, y: 50) 위치에서 (width : 1000, height : 500) 크기의 윈도우가 생성됩니다.
+        self.setGeometry(150, 50, 1000, 500)
         # Window의 Title을 지정합니다.
         self.UI()
 
@@ -14,16 +14,22 @@ class Window(QWidget):
         # 이미지를 삽일 할 때에도 QLabel을 이용합니다.
         self.image = QLabel(self)
         # QLabel의 속성에 보면 setPixmap이 있는데 이 값을 통하여 이미지를 지정합니다.
-        self.image.setPixmap(QPixmap("30837.jpg"))
-        self.image.move(150, 50)
+        self.image.setPixmap(QPixmap("image.png"))
+        self.image.move(50, 50)
 
         remove_button = QPushButton("remove", self)
-        remove_button.move(380, 400)
+        remove_button.move(380, 450)
         remove_button.clicked.connect(self.Remove)
 
         show_button = QPushButton("show", self)
-        show_button.move(500, 400)
+        show_button.move(500, 450)
         show_button.clicked.connect(self.Show)
+
+        self.checkbox = QCheckBox("체크박스", self)
+        self.checkbox.move(100,450)
+
+
+
 
         # Window를 띄웁니다.
         self.show()
