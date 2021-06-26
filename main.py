6f8d9b48cf3d57ad8eee4e0e622869320dc4630a
LDY <74583481+dy858@users.noreply.github.com>
2021-06-26 17:55:58 +0900

    0626

--- main.py
+++ main.py
@@ -1,49 +1,81 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QLabel
-
-class MyApp(QWidget):
-
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle('My First Application')
-        self.move(300, 300)
-        self.resize(900, 500)
+import cv2
+import os
+import ai
 
 
-        self.pixmap = QPixmap('image.png')
-        self.label = QLabel(self)
-        self.label.setPixmap(self.pixmap)  # 이미지 세팅
-        self.label.setContentsMargins(10, 10, 10, 10)
+def video_processing(video_path, background):
+    face_mask_recognition_model = cv2.dnn.readNet('models/face_mask_recognition.prototxt',
+                                                  'models/face_mask_recognition.caffemodel')
+    mask_detector_model = ai.create_model()
 
+    cap = cv2.VideoCapture(video_path)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    now_frame = 1
 
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 
-        remove_button = QPushButton("remove", self)
-        remove_button.move(380, 450)
-        remove_button.clicked.connect(self.Remove)
+    if not os.path.exists('outputs'):
+        os.mkdir('outputs')
 
-        show_button = QPushButton("show", self)
-        show_button.move(500, 450)
-        show_button.clicked.connect(self.Show)
+    out = None
 
-        self.checkbox = QCheckBox("체크박스", self)
-        self.checkbox.move(100, 450)
+    colors = [(0, 255, 0), (0, 0, 255)]
+    labels = ['with_mask', 'without_mask']
 
-        self.show()
+    while cap.isOpened():
+        ret, image = cap.read()
+        if not ret:
+            break
 
-    def Remove(self):
-        self.image.close()
+        h, w = image.shape[:2]
 
-    def Show(self):
-        self.image.show()
+        blob = cv2.dnn.blobFromImage(image, scalefactor=1., size=(300, 300), mean=(104., 177., 123.))
+        face_mask_recognition_model.setInput(blob)
+        dets = face_mask_recognition_model.forward()
+
+        result_image = image.copy()
+
+        for i in range(dets.shape[2]):
+            confidence = dets[0, 0, i, 2]
+            if confidence < 0.5:
+                continue
+
+            left = int(dets[0, 0, i, 3] * w)
+            top = int(dets[0, 0, i, 4] * h)
+            right = int(dets[0, 0, i, 5] * w)
+            bottom = int(dets[0, 0, i, 6] * h)
+
+            face = image[top:bottom, left:right]
+
+            face_image = image[top:bottom, left:right]
+            face_image = cv2.resize(face_image, dsize=(224, 224))
+            face_image = cv2.cvtColor(face_image, cv2.COLOR_BGR2RGB)
+
+            predict = ai.predict(mask_detector_model, face_image)
+
+            cv2.rectangle(result_image, pt1=(left, top), pt2=(right, bottom), thickness=2, color=colors[predict],
+                          lineType=cv2.LINE_AA)
+            cv2.putText(result_image, text=labels[predict], org=(left, top - 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale=0.8,
+                        color=colors[predict], thickness=2, lineType=cv2.LINE_AA)
+
+        if out is None:
+            out = cv2.VideoWriter('outputs/output.wmv', fourcc, cap.get(cv2.CAP_PROP_FPS),
+                                  (image.shape[1], image.shape[0]))
+        else:
+            out.write(result_image)
+
+        print('(' + str(now_frame) + '/' + str(frame_count) + '): ' + str(now_frame * 100 // frame_count) + '%')
+        now_frame += 1
+
+        if not background:
+            cv2.imshow('result', result_image)
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+    out.release()
+    cap.release()
 
 
 if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   ex = MyApp()
-   sys.exit(app.exec_())
+    video_processing('data/04.mp4', False)
